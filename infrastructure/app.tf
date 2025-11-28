2cdee06f1900ea79ae5f678624d735c2af5c2259
Jason Johnson <jasojohnson@microsoft.com>
2025-11-28 19:14:02 +0100

    Add Azure Key Vault and role assignments for application secrets management

---
+++ infrastructure/app.tf
@@ -0,0 +1,51 @@
+resource "azurerm_user_assigned_identity" "app" {
+  name                = provider::namep::namestring("azurerm_user_assigned_identity", local.namep_config, { name = "app" })
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+}
+
+locals {
+  app_name = provider::namep::namestring("azurerm_linux_web_app", local.namep_config, { name = "app" })
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = local.app_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_service_plan.main.location
+  service_plan_id     = azurerm_service_plan.main.id
+
+  identity {
+    type         = "SystemAssigned, UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.app.id]
+  }
+
+  site_config {
+    application_stack {
+      docker_image_name   = "mcr.microsoft.com/azure-sdk/azure-mcp:2.0.0-beta.7"
+      docker_registry_url = "https://mcr.microsoft.com"
+    }
+
+    always_on = false
+  }
+
+  app_settings = {
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.main.connection_string
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"   = "false"
+  }
+
+  logs {
+    detailed_error_messages = true
+    failed_request_tracing  = true
+
+    application_logs {
+      file_system_level = "Information"
+    }
+
+    http_logs {
+      file_system {
+        retention_in_days = 7
+        retention_in_mb   = 35
+      }
+    }
+  }
+}
---
+++ infrastructure/keyvault.tf
@@ -0,0 +1,28 @@
+resource "azurerm_key_vault" "main" {
+  name                       = provider::namep::namestring("azurerm_key_vault", local.namep_config)
+  location                   = var.location
+  resource_group_name        = azurerm_resource_group.main.name
+  sku_name                   = "standard"
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  enable_rbac_authorization  = true
+}
+
+resource "azurerm_role_assignment" "managed_admin" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "managed_secrets" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "app_secrets" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_linux_web_app.app.identity[0].principal_id
+}
