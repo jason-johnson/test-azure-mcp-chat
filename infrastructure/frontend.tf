e65dc0333cbf5df1fe6b3d305cdf1bade08fd84d
Jason Johnson <jasojohnson@microsoft.com>
2025-12-03 15:42:05 +0100

    Revert Python version to 3.13 in frontend configuration

--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -11,7 +11,7 @@ resource "azurerm_linux_web_app" "python_app" {
 
   site_config {
     application_stack {
-      python_version = "3.14"
+      python_version = "3.13"
     }
 
     app_command_line = "gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 agent:app"
