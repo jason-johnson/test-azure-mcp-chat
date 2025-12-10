2a325013d3d8d2d0ebd55e348e1455505d905a2e
Jason Johnson <jasojohnson@microsoft.com>
2025-12-10 13:24:15 +0100

    Remove deprecated deployment and environment variable scripts for Azure App Service

--- set-runscript.sh
+++
@@ -1,4 +0,0 @@
-az webapp config set \
-    --startup-file "gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 agent:app" \
-    --name $APP_SERVICE_NAME \
-    --resource-group $RESOURCE_GROUP_NAME
